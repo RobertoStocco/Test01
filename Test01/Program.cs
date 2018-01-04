d9c53fa622dd5c6c5467b5bb03179fb9dec9baf0
RobertoStocco <roberto.stocco@outlook.com>
2018-01-04 14:57:52 +0100

    1)Configurata la soluzione per usare NUnit. 2)Aggiunto progetto Test01Tests per i test.

--- Test01/Program.cs
+++ Test01/Program.cs
@@ -2,13 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Test01
 {
     class Program
     {
+        //public delegate TResult Func<Targ0, TResult>(Targ0 arg0);
+
         static void Main(string[] args)
         {
+            Predicate<string> isEmpty = x => string.Empty.Equals(x);
+            Action<object> writeAndRead = x => { Console.WriteLine(x); Console.ReadLine(); };
+            Func<string, string, bool> areStringsEmpty = (str1, str2) => isEmpty(str1) && isEmpty(str2);
+
+            //writeAndRead(areStringsEmpty(string.Empty, ""));
+
+            Action<string, int> waitForMessage = async (str, time) =>
+            {
+                writeAndRead(string.Format("Attendi {0}ms...", time));
+                await Task.Delay(time);
+                writeAndRead(str);
+            };
+
+            waitForMessage("Fatto!", 9000);
+
+            writeAndRead("");
+        }
+
+        static async Task aspetta(int millisecondi)
+        {
+            await Task.Delay(millisecondi);
         }
     }
 }
---
+++ Test01Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test01Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void VerificaEsecuzioneTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
